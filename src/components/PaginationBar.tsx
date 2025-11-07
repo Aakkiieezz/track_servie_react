f91ba3ff7b39c748d9e10b8ae3f9678925fee83e
Aakkiieezz <kadoleakash96@gmail.com>
2025-11-08 00:41:06 +0530

    improved styling of paginationBar

--- src/components/PaginationBar.tsx
+++ src/components/PaginationBar.tsx
@@ -26,101 +26,93 @@ const PaginationBar: React.FC<PaginationProps> = ({ pageNumber: pgNo, totalPages
     };
 
     return (
-        <nav id="paginationNav">
+        <nav className={styles.paginationNav}>
             {totalPages > 1 && (
-                // <ul className={`${styles.pagination} justify-content-center`}>
-                <ul className={`pagination justify-content-center ${styles.customPagination}`}>
+                <ul className="pagination justify-content-center">
 
-                    {/* Previous button */}
-                    <li className={`page-item ${!hasPrevious ? 'disabled' : ''}`}>
-                        <button
-                            className={`page-link ${styles.pageLink}`}
-                            onClick={(event) => handlePageChange(pgNo - 1, event)}
-                            disabled={!hasPrevious}>
-                            Previous
-                        </button>
+                {/* Previous */}
+                <li className={`page-item ${!hasPrevious ? 'disabled' : ''}`}>
+                    <button
+                        className={`${styles.pageLink} ${styles.prevNext}`}
+                        onClick={(event) => handlePageChange(pgNo - 1, event)}
+                        disabled={!hasPrevious}
+                    >
+                        Previous
+                    </button>
+                </li>
+
+                {pgNo !== 0 && (
+                    <li className="page-item">
+                    <button className={styles.pageLink} onClick={(event) => handlePageChange(0, event)}>1</button>
                     </li>
+                )}
 
-                    {/* First Page */}
-                    {pgNo !== 0 && (
-                        <li className="page-item">
-                            <button className={`page-link ${styles.pageLink}`} onClick={(event) => handlePageChange(0, event)}>1</button>
-                        </li>
-                    )}
-
-                    {/* Gap if current page is greater than 3 */}
-                    {pgNo > 3 && (
-                        <li className="page-item">
-                            <span className={`page-link ${styles.pageLink} disabled`}>...</span>
-                        </li>
-                    )}
-
-                    {/* Previous Pages (current - 2) */}
-                    {pgNo > 2 && (
-                        <li className="page-item">
-                            <button className={`page-link ${styles.pageLink}`} onClick={(event) => handlePageChange(pgNo - 2, event)}>
-                                {pgNo - 1}
-                            </button>
-                        </li>
-                    )}
-
-                    {/* Previous Page (current - 1) */}
-                    {pgNo > 1 && (
-                        <li className="page-item">
-                            <button className={`page-link ${styles.pageLink}`} onClick={(event) => handlePageChange(pgNo - 1, event)}>
-                                {pgNo}
-                            </button>
-                        </li>
-                    )}
-
-                    {/* Current Page */}
-                    <li className="page-item active">
-                        <button className={`page-link ${styles.pageLink}`}>{pgNo + 1}</button>
+                {pgNo > 3 && (
+                    <li className="page-item">
+                    <span className={`${styles.pageLink} ${styles.ellipsis}`}>...</span>
                     </li>
+                )}
 
-                    {/* Next Page (current + 1) */}
-                    {pgNo < totalPages - 2 && (
-                        <li className="page-item">
-                            <button className={`page-link ${styles.pageLink}`} onClick={(event) => handlePageChange(pgNo + 1, event)}>
-                                {pgNo + 2}
-                            </button>
-                        </li>
-                    )}
-
-                    {/* Next Next Page (current + 2) */}
-                    {pgNo < totalPages - 3 && (
-                        <li className="page-item">
-                            <button className={`page-link ${styles.pageLink}`} onClick={(event) => handlePageChange(pgNo + 2, event)}>
-                                {pgNo + 3}
-                            </button>
-                        </li>
-                    )}
-
-                    {/* Gap if current page is less than totalPages - 4 */}
-                    {pgNo < totalPages - 4 && (
-                        <li className="page-item">
-                            <span className={`page-link ${styles.pageLink} disabled`}>...</span>
-                        </li>
-                    )}
-
-                    {/* Last Page */}
-                    {pgNo !== totalPages - 1 && (
-                        <li className="page-item">
-                            <button className={`page-link ${styles.pageLink}`} onClick={(event) => handlePageChange(totalPages - 1, event)}>
-                                {totalPages}
-                            </button>
-                        </li>
-                    )}
-
-                    {/* Next button */}
-                    <li className={`page-item ${!hasNext ? 'disabled' : ''}`}>
-                        <button
-                            className={`page-link ${styles.pageLink}`}
-                            onClick={(event) => handlePageChange(pgNo + 1, event)}
-                            disabled={!hasNext}>
-                            Next
-                        </button>
+                {pgNo > 2 && (
+                    <li className="page-item">
+                    <button className={styles.pageLink} onClick={(event) => handlePageChange(pgNo - 2, event)}>
+                        {pgNo - 1}
+                    </button>
                     </li>
+                )}
+
+                {pgNo > 1 && (
+                    <li className="page-item">
+                    <button className={styles.pageLink} onClick={(event) => handlePageChange(pgNo - 1, event)}>
+                        {pgNo}
+                    </button>
+                    </li>
+                )}
+
+                <li className="page-item active">
+                    <button className={styles.pageLink}>{pgNo + 1}</button>
+                </li>
+
+                {pgNo < totalPages - 2 && (
+                    <li className="page-item">
+                    <button className={styles.pageLink} onClick={(event) => handlePageChange(pgNo + 1, event)}>
+                        {pgNo + 2}
+                    </button>
+                    </li>
+                )}
+
+                {pgNo < totalPages - 3 && (
+                    <li className="page-item">
+                    <button className={styles.pageLink} onClick={(event) => handlePageChange(pgNo + 2, event)}>
+                        {pgNo + 3}
+                    </button>
+                    </li>
+                )}
+
+                {pgNo < totalPages - 4 && (
+                    <li className="page-item">
+                    <span className={`${styles.pageLink} ${styles.ellipsis}`}>...</span>
+                    </li>
+                )}
+
+                {pgNo !== totalPages - 1 && (
+                    <li className="page-item">
+                    <button className={styles.pageLink} onClick={(event) => handlePageChange(totalPages - 1, event)}>
+                        {totalPages}
+                    </button>
+                    </li>
+                )}
+
+                <li className={`page-item ${!hasNext ? 'disabled' : ''}`}>
+                    <button
+                        className={`${styles.pageLink} ${styles.prevNext}`}
+                        onClick={(event) => handlePageChange(pgNo + 1, event)}
+                        disabled={!hasNext}
+                    >
+                        Next
+                    </button>
+                </li>
+
                 </ul>
             )}
         </nav>
